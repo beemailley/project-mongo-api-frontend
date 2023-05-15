089bae8437538ff44f4f8f39d49c7535790d3cde
Bridget Mailley <bmailley42@gmail.com>
2023-05-15 09:41:22 +0200

    updated documentation

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -17,7 +17,7 @@ export const Documentation = () => {
         <ul>
           <li>platform: filters by the platform the game was released for</li>
           <ul>
-            <li>This filter is case sensitive.</li>
+            <li>This filter is not case sensitive.</li>
             <li>The values of platform must be one of the following:</li>
             <ul>
               <li>3DS</li>
@@ -36,7 +36,7 @@ export const Documentation = () => {
         <ul>
           <li>rating: filters by the ESRB rating for the game</li>
           <ul>
-            <li>This filter is case sensitive.</li>
+            <li>This filter is not case sensitive.</li>
             <li>The values of platform must be one of the following:</li>
             <ul>
               <li>E</li>
@@ -48,19 +48,12 @@ export const Documentation = () => {
             </ul>
             <li>Example: /games?rating=E</li>
           </ul>
+          <ul />
+          <li>releaseyear: filters by the year in which the game was released</li>
           <ul>
-            <li>releaseyear: filters by the year in which the game was released</li>
-            <ul>
-              <li>This filter must include 4 digits.</li>
-              <li>The values of platform can be any year between 1996 adn 2021, inclusive.</li>
-              <li>Example: /games?releaseyear=1996</li>
-            </ul>
-          </ul>
-          <li>author: filters by author name;</li>
-          <ul>
-            <li>This query accepts both exact author names and partial author names.</li>
-            <li>Example: /games?author=alice</li>
-            <li>Example: /games?author=Amrou Al-Kadhi</li>
+            <li>This filter must include 4 digits.</li>
+            <li>The values of platform can be any year between 1996 adn 2021, inclusive.</li>
+            <li>Example: /games?releaseyear=1996</li>
           </ul>
         </ul>
         <li>Use <Link href="https://project-mongo-api-wjvhdzdcja-lz.a.run.app/games">/games</Link> </li>
